86e6d5d091b6577fa3714c1edb64a417d5a34bd0
Nanda <Nanda.Adapa@fcsamerica.com>
2025-09-03 12:37:40 -0500

    sadf

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: []
+})
+export class AppModule { }
