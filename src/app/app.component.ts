2ae006d1e10ceb2dfc7b98ee6d6d294d5a7aff61
Nanda <Nanda.Adapa@fcsamerica.com>
2025-09-03 13:03:18 -0500

    add ngx echarts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-angular-app';
+    barChartOptions = {
+    title: { text: 'Monthly Sales' },
+    tooltip: {},
+    xAxis: { type: 'category', data: ['Jan', 'Feb', 'Mar', 'Apr'] },
+    yAxis: { type: 'value' },
+    series: [{ type: 'bar', data: [120, 200, 150, 80] }]
+  };
+
+  pieChartOptions = {
+    title: { text: 'Market Share', left: 'center' },
+    tooltip: { trigger: 'item' },
+    legend: { orient: 'vertical', left: 'left' },
+    series: [{
+      type: 'pie',
+      radius: '50%',
+      data: [
+        { value: 40, name: 'Brand A' },
+        { value: 30, name: 'Brand B' },
+        { value: 20, name: 'Brand C' },
+        { value: 10, name: 'Brand D' }
+      ]
+    }]
+  };
+
+  lineChartOptions = {
+    title: { text: 'Website Traffic' },
+    tooltip: { trigger: 'axis' },
+    xAxis: { type: 'category', data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'] },
+    yAxis: { type: 'value' },
+    series: [{ type: 'line', data: [820, 932, 901, 934, 1290] }]
+  };
+
+  gaugeChartOptions = {
+    series: [{
+      type: 'gauge',
+      progress: { show: true },
+      detail: { valueAnimation: true, formatter: '{value}%' },
+      data: [{ value: 75, name: 'Completion' }]
+    }]
+  };
+
+  heatmapOptions = {
+    visualMap: {
+      min: 0,
+      max: 100,
+      calculable: true,
+      orient: 'horizontal',
+      left: 'center',
+      bottom: '15%'
+    },
+    xAxis: { type: 'category', data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'] },
+    yAxis: { type: 'category', data: ['Morning', 'Afternoon', 'Evening'] },
+    series: [{
+      type: 'heatmap',
+      data: [
+        [0, 0, 10], [0, 1, 20], [0, 2, 30],
+        [1, 0, 40], [1, 1, 50], [1, 2, 60],
+        [2, 0, 70], [2, 1, 80], [2, 2, 90]
+      ]
+    }]
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,64 @@
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { AppComponent } from './app.component';
+// import { AppRoutingModule } from './app-routing.module';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent
+//   ],
+//   bootstrap: [AppComponent],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule
+//   ],
+//   providers: []
+// })
+// export class AppModule { }
+
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxEchartsModule } from 'ngx-echarts';
+import * as echarts from 'echarts/core';
+
+// Import only the chart types and components you need
+import {
+  LineChart,
+  BarChart,
+  PieChart
+} from 'echarts/charts';
+
+import {
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent,
+  GridComponent
+} from 'echarts/components';
+
+import { CanvasRenderer } from 'echarts/renderers';
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+
+echarts.use([
+  LineChart,
+  BarChart,
+  PieChart,
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent,
+  GridComponent,
+  CanvasRenderer
+]);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  bootstrap: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    NgxEchartsModule.forRoot({ echarts })
   ],
-  providers: []
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
+
